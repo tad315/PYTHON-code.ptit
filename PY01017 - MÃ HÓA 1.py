0172550d84df99d876911bbb451b17ba747491ae
Trần Thị Ánh Dương <177900937+tad315@users.noreply.github.com>
2025-08-12 10:27:02 +0700

    Create PY01017 - MÃ HÓA 1.py

---
+++ PY01017 - MÃ HÓA 1.py
@@ -0,0 +1,13 @@
+t = int(input())
+for _ in range(t):
+    s = input().strip()
+    res = ""
+    cnt = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i - 1]:
+            cnt += 1
+        else:
+            res += str(cnt) + s[i - 1]
+            cnt = 1
+    res += str(cnt) + s[-1]
+    print (res)
