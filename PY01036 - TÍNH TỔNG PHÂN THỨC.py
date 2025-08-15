91ee5f3e98eeda56826ca8c09a5fa379970edcca
Trần Thị Ánh Dương <177900937+tad315@users.noreply.github.com>
2025-08-15 22:58:08 +0700

    Create PY01036 - TÍNH TỔNG PHÂN THỨC.py

---
+++ PY01036 - TÍNH TỔNG PHÂN THỨC.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = 0.0
+    if n % 2 == 0:
+        for i in range(2, n + 1, 2):
+            s += 1/i
+    else:
+        for i in range(1, n + 1, 2):
+            s += 1/i
+    print(f"{s:.6f}")
